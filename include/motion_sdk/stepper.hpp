2c1ad1bd00953a6bb02bd5f65d8f81639eadc496
kamal2730 <itzkamalkant@gmail.com>
2026-01-09 19:34:15 +0530

    feat:added stepper support
    integrated but not tested yetgit add .

---
+++ include/motion_sdk/stepper.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "motion_sdk/usb_serial.hpp"
+
+namespace motion_sdk {
+
+class Stepper {
+public:
+    Stepper(USBSerial& serial);
+
+    bool ping();
+    bool setPosition(float angle_deg);
+    bool getPosition(float& angle_deg);
+
+private:
+    USBSerial& serial_;
+};
+
+} // namespace motion_sdk
---
+++ src/stepper.cpp
@@ -0,0 +1,52 @@
+#include "motion_sdk/stepper.hpp"
+
+#include <sstream>
+
+namespace motion_sdk {
+
+Stepper::Stepper(USBSerial& serial)
+    : serial_(serial) {}
+
+bool Stepper::ping() {
+    if (!serial_.writeLine("PING"))
+        return false;
+
+    std::string resp;
+    if (!serial_.readLine(resp))
+        return false;
+
+    return resp == "OK";
+}
+
+bool Stepper::setPosition(float angle_deg) {
+    std::ostringstream cmd;
+    cmd << "SET_POS " << angle_deg;
+
+    if (!serial_.writeLine(cmd.str()))
+        return false;
+
+    std::string resp;
+    if (!serial_.readLine(resp))
+        return false;
+
+    return resp == "OK";
+}
+
+bool Stepper::getPosition(float& angle_deg) {
+    if (!serial_.writeLine("GET_POS"))
+        return false;
+
+    std::string resp;
+    if (!serial_.readLine(resp))
+        return false;
+
+    try {
+        angle_deg = std::stof(resp);
+    } catch (...) {
+        return false;
+    }
+
+    return true;
+}
+
+} // namespace motion_sdk
